6b58fad365334b304b7f1329826c465563680c2c
Dharmendra kumar <dharm117x@gmail.com>
2024-10-15 18:33:29 +0530

    add bootstrap nav and props

---
+++ firstapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+   <Navbar  page="HomePage"/>
+   <Navbar  page="HomePage" title='ReactJS'/>
+    
+    </>
+   
+  );
+}
+
+export default App;
