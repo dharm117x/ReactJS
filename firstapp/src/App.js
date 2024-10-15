c61ecbf430c12481f05b574aff9768b6d53d7054
Dharmendra kumar <dharm117x@gmail.com>
2024-10-15 19:01:29 +0530

    add text box with state and event

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForms from './components/TextForms';
 
 function App() {
   return (
-    <>
+  
+  <div className="container">
+
    <Navbar  page="HomePage"/>
    <Navbar  page="HomePage" title='ReactJS'/>
+   <TextForms heading="Text box"/>
+   </div>
     
-    </>
    
   );
 }
---
+++ firstapp/src/components/TextForms.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+
+export default function TextForms(props) {
+    const[text, setText] = useState('Enter text here');
+
+    //setText('Hello react World');
+
+    const toUpper = ()=>{
+        console.log('To upper case');
+        setText(text.toUpperCase())
+    }
+
+    const toUpdate = (event)=>{
+        console.log('To update data');
+        setText(event.target.value);
+    }
+    
+    return (
+    <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+            <textarea className='form-control' id='box1' value={text} onChange={toUpdate} rows={8}></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={toUpper}>To Uppercase</button>
+    </div>
+
+  );
+
+
+}
+
